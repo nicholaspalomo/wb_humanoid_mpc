cc99d31a7e387282befc74007295120e46fcc172
Nicholas Palomo <nicholas.palomo@gmail.com>
2026-08-30 16:19:42 -0700

    fix the license

--- humanoid_nmpc/humanoid_common_mpc/include/humanoid_common_mpc/acom/AngularCenterOfMass.h
+++ humanoid_nmpc/humanoid_common_mpc/include/humanoid_common_mpc/acom/AngularCenterOfMass.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-Copyright (c) 2026, wb_humanoid_mpc contributors. All rights reserved.
+Copyright (c) 2026, Nicholas Palomo. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
